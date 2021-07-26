d050de907f226f890b47784e05780052d0a62435
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-07-26 20:29:39 +0800

    feat: Add reducers

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { recordlistSlice } from "screens/main/recordlist.slice";
+import { recordfilterSlice } from "screens/main/recordfilter.slice";
+import { navigateSlice } from "screens/main/record/navigate.slice";
 
 export const rootReducer = {
   recordlistReducer: recordlistSlice.reducer,
+  recordfilterReducer: recordfilterSlice.reducer,
+  navigateReducer: navigateSlice.reducer,
 };
 
 export const store = configureStore({
