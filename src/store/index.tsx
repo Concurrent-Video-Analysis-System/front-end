759ef53e2a4b35a996c15d4664060b94c8c8d68d
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-08-13 16:45:16 +0800

    feat: Update `rootState` reducer store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { recordlistSlice } from "screens/main/recordlist.slice";
 import { recordfilterSlice } from "screens/main/recordfilter.slice";
 import { navigateSlice } from "screens/main/record/navigate.slice";
+import { deviceSlice } from "screens/main/device/device.slice";
+import { locationSlice } from "screens/main/device/location.slice";
 
 export const rootReducer = {
   recordlistReducer: recordlistSlice.reducer,
   recordfilterReducer: recordfilterSlice.reducer,
   navigateReducer: navigateSlice.reducer,
+  deviceReducer: deviceSlice.reducer,
+  locationReducer: locationSlice.reducer,
 };
 
 export const store = configureStore({
