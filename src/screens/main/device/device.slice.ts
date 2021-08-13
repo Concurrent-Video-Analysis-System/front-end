bfd6b8aacd06952c065f840336bcbd05564198d9
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-08-13 16:44:16 +0800

    feat: Add deviceReducer

---
+++ src/screens/main/device/device.slice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "store";
+
+export interface DeviceProps {
+  id: number;
+  location_id: number;
+  location_name: string;
+  viewport: string;
+  rtsp: string;
+}
+
+interface State {
+  deviceList: DeviceProps[];
+  isLoading: boolean;
+}
+
+const initialState: State = {
+  deviceList: [],
+  isLoading: false,
+};
+
+export const deviceSlice = createSlice({
+  name: "deviceSlice",
+  initialState: initialState,
+  reducers: {
+    set(state, action) {
+      state.deviceList = action.payload;
+    },
+    append(state, action) {
+      state.deviceList = [...state.deviceList, action.payload];
+    },
+    clear(state) {
+      state.deviceList = [];
+    },
+    setLoading(state, action) {
+      state.isLoading = action.payload;
+    },
+  },
+});
+
+export const selectDeviceReducer = (state: RootState) => state.deviceReducer;
