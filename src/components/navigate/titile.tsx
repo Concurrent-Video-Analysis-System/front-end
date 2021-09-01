a73228dc3d1005729c591612f37ed3396281703d
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-09-01 14:33:23 +0800

    feat: Implement global breadcrumb navigator

---
+++ src/components/navigate/titile.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import {
+  CameraOutlined,
+  ControlOutlined,
+  EnvironmentOutlined,
+  ExceptionOutlined,
+  GoldOutlined,
+  LineChartOutlined,
+  SaveOutlined,
+  ScheduleOutlined,
+  SettingOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+
+const findNodeInTree = (
+  treeRootNode: LocationTreeNodeProps,
+  predicate: (node: LocationTreeNodeProps) => unknown
+): LocationTreeNodeProps | undefined => {
+  if (predicate(treeRootNode)) {
+    return treeRootNode;
+  }
+  return treeRootNode.children.find((node) => findNodeInTree(node, predicate));
+};
+
+export const location2title = (location: string) => {
+  const tempRoot = {
+    key: "",
+    children: locationTree,
+  };
+  const result = findNodeInTree(
+    tempRoot,
+    (node) => node.key === location || node.key === "/" + location
+  );
+
+  if (!result || result === tempRoot) return undefined;
+  return result.title;
+};
+
+export interface LocationTreeNodeProps {
+  key: React.Key;
+  title?: string;
+  icon?: JSX.Element;
+  children: LocationTreeNodeProps[];
+}
+
+export const locationTree: LocationTreeNodeProps[] = [
+  {
+    key: "/dashboard",
+    title: "数据总览",
+    icon: <LineChartOutlined />,
+    children: [],
+  },
+  {
+    key: "device-menu",
+    title: "设备管理",
+    icon: <GoldOutlined />,
+    children: [
+      {
+        key: "/device",
+        title: "摄像头管理",
+        icon: <CameraOutlined />,
+        children: [],
+      },
+      {
+        key: "/device/location",
+        title: "网点管理",
+        icon: <EnvironmentOutlined />,
+        children: [],
+      },
+    ],
+  },
+  {
+    key: "task-menu",
+    title: "监察任务管理",
+    icon: <ScheduleOutlined />,
+    children: [
+      {
+        key: "/task",
+        title: "实时监控分析",
+        icon: <VideoCameraOutlined />,
+        children: [],
+      },
+      {
+        key: "/task1",
+        title: "历史录像分析",
+        icon: <SaveOutlined />,
+        children: [],
+      },
+    ],
+  },
+  {
+    key: "/record",
+    title: "违规记录管理",
+    icon: <ExceptionOutlined />,
+    children: [],
+  },
+  {
+    key: "system-menu",
+    title: "系统管理",
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: "/user",
+        title: "用户信息",
+        icon: <UserOutlined />,
+        children: [],
+      },
+      {
+        key: "/settings",
+        title: "分析配置",
+        icon: <ControlOutlined />,
+        children: [],
+      },
+    ],
+  },
+];
--- src/screens/main/breadcrumb.tsx
+++ src/screens/main/breadcrumb.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Breadcrumb } from "antd";
 import { HomeOutlined } from "@ant-design/icons";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
+import { location2title } from "../../components/navigate/titile";
 
 export interface BreadcrumbNavigatorProps {
   icon?: JSX.Element;
@@ -12,16 +13,46 @@ export const BreadcrumbNavigator = (props: BreadcrumbNavigatorProps) => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const locationCascade = useMemo(() => {
+    return location.pathname
+      .split("/")
+      .reduce(
+        (prev, item) => [
+          ...prev,
+          {
+            title: location2title(item),
+            path: (prev[prev.length - 1]?.path || "") + item + "/",
+          },
+        ],
+        [] as { path: string; title: string | undefined }[]
+      )
+      .map((item) =>
+        item.path.length > 1
+          ? {
+              ...item,
+              path: item.path.slice(0, item.path.length - 1),
+            }
+          : item
+      );
+  }, [location]);
+
+  console.log(locationCascade);
+
   return (
     <Container>
       {props.icon}
       <Breadcrumb>
-        <Breadcrumb.Item>
-          <HomeOutlined />
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>
-          <Link to={"/recordlist"}>违规行为列表</Link>
-        </Breadcrumb.Item>
+        {locationCascade.map((item) => (
+          <Breadcrumb.Item>
+            {item.path === "/" ? (
+              <HomeOutlined />
+            ) : (
+              <BreadcrumbLink onClick={() => navigate(item.path)}>
+                {item.title}
+              </BreadcrumbLink>
+            )}
+          </Breadcrumb.Item>
+        ))}
       </Breadcrumb>
     </Container>
   );
@@ -30,6 +61,18 @@ export const BreadcrumbNavigator = (props: BreadcrumbNavigatorProps) => {
 const Container = styled.div`
   width: 100%;
   height: calc(100% - 1px); // 1px for border-bottom
-  padding: 1rem 2rem;
   border-bottom: 1px solid #e0e0e0;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  padding: 0 2rem;
+`;
+
+const BreadcrumbLink = styled.span`
+  user-select: none;
+  cursor: pointer;
+  :hover {
+    color: #1890ff;
+  }
+  transition: 0.3s;
 `;
