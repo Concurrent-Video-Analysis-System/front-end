df9c681c030a587579d7f3ff2161e4c5b956ac47
Criheacy <hekaiqi@mail.sdu.edu.cn>
2021-09-04 23:51:28 +0800

    feat(filter.ts): Implement new filter hook

---
+++ src/utils/filter.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { useUrlQueryParams } from "./url";
+import { useHttp } from "./http";
+import { message } from "antd";
+
+export const useFilter = <K extends string>(
+  fetchEndpoint: string = "",
+  filterPropsName: K[]
+) => {
+  const [filterProps, setFilterPropsRaw] = useState(
+    Object.fromEntries(
+      filterPropsName.map((prop) => [prop, undefined as unknown])
+    )
+  );
+
+  // FIXME: `setFilterProps` won't execute directly
+  const setFilterProps = (name: K, value: unknown) => {
+    setFilterPropsRaw({ ...filterProps, [name]: value });
+  };
+
+  const [urlParams, setUrlParams] = useUrlQueryParams(filterPropsName);
+  const [responseData, setResponseData] = useState<unknown>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendHttp = useHttp();
+
+  useEffect(() => {
+    setIsLoading(true);
+    sendHttp(fetchEndpoint, { method: "GET", data: filterProps })
+      .then((response) => {
+        setIsLoading(false);
+        setResponseData(response.data);
+      })
+      .catch((errorMessage) => {
+        setIsLoading(false);
+        message.error(`更新列表时出错：${errorMessage}`).then(null);
+      });
+    setUrlParams(filterProps);
+  }, [filterProps]);
+
+  return {
+    filterProps,
+    setFilterProps,
+    urlParams,
+    responseData,
+    isLoading,
+  } as const;
+};
